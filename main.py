f35c5119f14ac67ce7b2bc923b4c2e06cf4e0c6d
Benhadjhassiner <144700853+Benhadjhassiner@users.noreply.github.com>
2023-09-22 10:36:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ def count_word(phrase): #L'input est la phrase que l'utilisateur donnera, ici la
     #La fonction va compter le nombre de mots d'une chaîne de caractères
     mots = phrase.count(" ")
     return mots + 1 
-    #L'output est le nombre de mots qu'on retournera, ici la variable "mots" auquelle on ajoute 1
+    #L'output est un nombre de mots qu'on retournera, ici la variable "mots" auquelle on ajoute 1
