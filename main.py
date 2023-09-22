399549edb89196fb5725dc3b7fe951abd4dbffe0
Benhadjhassiner <144700853+Benhadjhassiner@users.noreply.github.com>
2023-09-22 10:28:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-# Création de la fonction count_word
-def count_word():
-    return (len(phrase.split(' ')))
+# Définition de la fonction count_word
 
-# On demande à l'utilisateur sa phrase
-phrase = str(input("Écrivez une phrase afin d'en compter le nombre de mots: "))
-
-# Création de la variable pour la f-string
-reponse = f"Votre chaîne de caractères contient {count_word()} mots et onomatopées."
-
-# On donne le nombre de mots dans la phrase de l'utilisateur
-print(reponse)
+def count_word(phrase):
+    (phrase.strip())
+    mots = (phrase.count(" "))
+    print(mots + 1)
