7e04a90136581c0cb4667b568245aa81472cda59
Benhadjhassiner <144700853+Benhadjhassiner@users.noreply.github.com>
2023-09-27 13:43:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# Définition de la fonction count_word
-
-def count_word(phrase): #L'input est la phrase que l'utilisateur donnera, ici la variable "phrase"
+#La fonction compte le nombre de mots d'une chaîne de caractères de type string. L'input est la phrase qu'on rentre et l'output est le nombre de mots qui nous sera retourné
+def count_word(phrase):
     phrase.strip()
-    #La fonction va compter le nombre de mots d'une chaîne de caractères
     mots = phrase.count(" ")
     return mots + 1 
-    #L'output est un nombre de mots qu'on retournera, ici la variable "mots" auquelle on ajoute 1
+    
